444cf6e7ba2f54640d2eccc725e90490f983bced
david <davidelrg@gmail.com>
2021-11-15 20:19:32 -0500

    V0.995

--- src/main/java/ucf/assignments/listOfList.java
+++ src/main/java/ucf/assignments/listOfList.java
@@ -5,7 +5,6 @@
 package ucf.assignments;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class listOfList {
 
@@ -17,23 +16,9 @@ public class listOfList {
         arr.add(newToDoList);
     }
 
-    public  void deleteList(int index) {
-
-        arr.remove(index);
-    }
-
     public  toDoList getList(int index){
 
         return arr.get(index);
-    }
-
-    public  int isEmpty(){
-
-        if(arr.isEmpty())
-            return 1;
-
-        else
-            return 0;
     }
 
     public int findListIndex(String title){
@@ -46,70 +31,4 @@ public class listOfList {
 
         return -1;
     }
-    
-    public  int exportOne(){
-
-        /*
-       This method will read from a file
-
-       It will get a list from the file
-
-       the list will be added to the listoflist object
-
-       Will return 1 if it completes the task
-
-       else will return 0
-
-       */
-
-
-
-        return -1;
-    }
-
-    public  int exportAll(){
-
-        /*
-       This method will read all the existing list from an external file
-
-       it will combine it with the current listOfList
-
-       Will return 1 if it completes the task
-
-       else will return 0
-
-       */
-        return -1;
-    }
-
-    public  int importOne(){
-
-        /*
-       This method will take an int along with getList() to save the list items to an external file
-
-       Will return 1 if it completes the task
-
-       else will return 0
-
-       */
-
-
-        return -1;
-    }
-
-    public  int importAll(){
-
-        /*
-       This method will save all the list items to an external file
-
-       Will return 1 if it completes the task
-
-       else will return 0
-
-       */
-
-
-        return -1;
-    }
-
 }
---
+++ src/test/java/ucf/assignments/ControllerMainTest.java
@@ -0,0 +1,40 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerMainTest {
+
+    @Test
+    void deleteItem() {
+    }
+
+    @Test
+    void clearAll() {
+    }
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void export() {
+    }
+
+    @Test
+    void listImport() {
+    }
+
+    @Test
+    void newItemWindow() {
+    }
+
+    @Test
+    void setDone() {
+    }
+
+    @Test
+    void itemEdit() {
+    }
+}
